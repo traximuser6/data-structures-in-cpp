b1796c6b92ff9dab9d912497f566c24d24dc654e
ddanish <traximuser6@gmail.com>
2025-09-22 11:17:52 -0700

    the very first comit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
-
 using namespace std;
 
+class BankAccount {
+private:
+    double balance;
+
+public:
+    explicit BankAccount(double openingBalance)
+        : balance(openingBalance) {
+    }
+
+    void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            cout << "Invalid deposit amount!\n";
+        }
+    }
+
+    void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            cout << "Invalid or insufficient funds!\n";
+        }
+    }
+
+    void display() const {
+        cout << "Current balance: " << balance << "\n";
+    }
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
+    BankAccount ba1(100.00); // create account
+
+    cout << "Before transactions\n";
+    ba1.display();
+
+    ba1.deposit(74.35); // make deposit
+    ba1.withdraw(20.00); // make withdrawal
+
+    cout << "After transactions\n";
+    ba1.display();
+
     return 0;
 }
